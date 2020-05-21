f524e0c36dd34c1d93e3aaf8b909a474ba72002b
Jassu Sharma <jassusharma660@gmail.com>
2020-05-21 17:46:37 +0530

    Create footer.php

---
+++ bin/includes/footer.php
@@ -0,0 +1,11 @@
+        <div id="footer">
+            <span class="links">
+                <a href="#">Report a problem</a>
+                <a href="#">T & C</a>
+                <a href="#">Feedback</a>
+                <a href="#">About</a>
+            </span>
+            <span href="#" class="copyright">
+                <a><?php echo date('Y');?></a>
+            </span>
+        </div>
