732d8f0b3c9291bdb9a7706aa000c809ed5dc5ad
Jassu Sharma <jassusharma660@gmail.com>
2020-05-21 17:48:50 +0530

    Create DIR.php

---
+++ DIR.php
@@ -0,0 +1,45 @@
+<?php
+    $base_url = './';
+    
+    $base_bin = $base_url.'bin/';
+    $base_includes = $base_bin.'includes/';
+
+    $base_config = $base_url.'config/';
+
+    ////////////////////////////////////
+    
+    $bin = './bin/';
+    $includes = $bin.'includes/';
+
+    $config = './config/';
+
+    $layout = $base_url.'layout/';
+
+    $css = $layout.'css/';
+    $script = $layout.'script/';
+    $images = $layout.'images/';
+    $fonts = $layout.'fonts/';
+
+    $banners = $images.'banners/';
+    $logos = $images.'logos/';
+    $favicon = $images.'favicon/';
+    $icons = $images.'icons/';
+
+    $userdata = './userdata/';
+    $profile_pics = $userdata.'profile-pics/';
+    $full_profile_pics = $profile_pics.'full/';
+    $medium_profile_pics = $profile_pics.'medium/';
+    $small_profile_pics = $profile_pics.'small/';
+    $year_profile_pics = $profile_pics.'year/';
+
+    
+    ///////////////////moo/buddy/////////////////
+    $mb = 'http://moobuddy.com/';
+    $mb_userdata = $mb.'userdata/';
+
+    $mb_profile_pics = $mb_userdata.'profile-pics/';
+    $mb_full_profile_pics = $mb_profile_pics.'full/';
+    $mb_medium_profile_pics = $mb_profile_pics.'medium/';
+    $mb_small_profile_pics = $mb_profile_pics.'small/';
+    $mb_year_profile_pics = $mb_profile_pics.'year/';
+?>
